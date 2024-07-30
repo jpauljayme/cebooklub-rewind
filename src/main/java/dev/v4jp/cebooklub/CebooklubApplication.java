f7e06be6d01f826743c4b25651f80c04e91564e5
John Paul Jayme <john.jayme@nextbank.ph>
2024-07-30 21:30:47 +0800

    Initial commit

---
+++ src/main/java/dev/v4jp/cebooklub/CebooklubApplication.java
@@ -0,0 +1,13 @@
+package dev.v4jp.cebooklub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CebooklubApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CebooklubApplication.class, args);
+  }
+
+}
---
+++ src/test/java/dev/v4jp/cebooklub/CebooklubApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.v4jp.cebooklub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CebooklubApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
