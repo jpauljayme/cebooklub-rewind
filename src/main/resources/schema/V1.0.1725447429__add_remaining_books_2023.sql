9ddcc5a8a29e383bfd660cc8333c15a98d07ff8a
John Paul Jayme <jpaul.jayme@gmail.com>
2024-09-05 10:32:19 +0800

    [feature/books-list-view] Fixing hero text

---
+++ src/main/resources/schema/V1.0.1725447429__add_remaining_books_2023.sql
@@ -0,0 +1,65 @@
+INSERT INTO tbl_book
+(title, author, body, image_path, date_read, t_date_created, t_date_updated)
+VALUES
+('Kafka On The Shore',
+ 'Haruki Murakami',
+ 'A metaphysical tale intertwining reality and the surreal, where a young boy named Kafka runs away from home to escape a family curse.',
+ 'june_2023.jpg',
+ CURRENT_DATE,
+ CURRENT_TIMESTAMP,
+ CURRENT_TIMESTAMP);
+
+INSERT INTO tbl_book
+(title, author, body, image_path, date_read, t_date_created, t_date_updated)
+VALUES
+('No Longer Human',
+ 'Osamu Dazai',
+ 'A harrowing story of alienation and existential despair, reflecting the author''s own struggles with depression and identity.',
+ 'july_2023.jpg',
+ CURRENT_DATE,
+ CURRENT_TIMESTAMP,
+ CURRENT_TIMESTAMP);
+
+INSERT INTO tbl_book
+(title, author, body, image_path, date_read, t_date_created, t_date_updated)
+VALUES
+('Fooled by Randomness',
+ 'Nassim Nicholas Taleb',
+ 'A compelling exploration of the role of luck and randomness in financial markets and everyday life, challenging our understanding of probability and success.',
+ 'august_2023.jpg',
+ CURRENT_DATE,
+ CURRENT_TIMESTAMP,
+ CURRENT_TIMESTAMP);
+
+INSERT INTO tbl_book
+(title, author, body, image_path, date_read, t_date_created, t_date_updated)
+VALUES
+('Eating Fire and Drinking Water',
+ 'Arlene J. Chai',
+ 'A gripping narrative set in a fictional Southeast Asian country, examining political intrigue and personal revelations.',
+ 'september_2023.jpg',
+ CURRENT_DATE,
+ CURRENT_TIMESTAMP,
+ CURRENT_TIMESTAMP);
+
+INSERT INTO tbl_book
+(title, author, body, image_path, date_read, t_date_created, t_date_updated)
+VALUES
+('This Is How You Lose the Time War',
+ 'Amal El-Mohtar and Max Gladstone',
+ 'A unique sci-fi love story told through letters between two rival agents from warring futures, blending poetry and espionage.',
+ 'october_2023.jpg',
+ CURRENT_DATE,
+ CURRENT_TIMESTAMP,
+ CURRENT_TIMESTAMP);
+
+INSERT INTO tbl_book
+(title, author, body, image_path, date_read, t_date_created, t_date_updated)
+VALUES
+('Flowers for Algernon',
+ 'Daniel Keyes',
+ 'A thought-provoking science fiction novel about a mentally disabled man who undergoes an experiment to increase his intelligence, leading to unexpected consequences.',
+ 'november_2023.jpg',
+ CURRENT_DATE,
+ CURRENT_TIMESTAMP,
+ CURRENT_TIMESTAMP);
